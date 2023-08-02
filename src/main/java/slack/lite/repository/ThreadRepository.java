571f8b754081797d89ac33ea76136a3f434210d8
Angelisium <angelisium@daum.net>
2023-08-02 19:35:45 +0200

    Update ThreadRepository.java

--- src/main/java/slack/lite/repository/ThreadRepository.java
+++ src/main/java/slack/lite/repository/ThreadRepository.java
@@ -1,8 +1,10 @@
 package slack.lite.repository;
 
+import java.util.Set;
 import java.util.UUID;
 import slack.lite.entity.Thread;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ThreadRepository extends JpaRepository<Thread, UUID> {
+	Set<Thread> findAllByOrderByLabelAsc();
 }
