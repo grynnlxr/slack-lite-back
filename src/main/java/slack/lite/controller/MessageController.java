c316124533ae5274328cde6885ee586a80b209f9
Fab <fabien@elixeer.fr>
2023-08-02 12:28:49 +0200

    message Service et controller ok

--- src/main/java/slack/lite/controller/MessageController.java
+++ src/main/java/slack/lite/controller/MessageController.java
@@ -1,32 +1,33 @@
 package slack.lite.controller;
 
-import org.aspectj.bridge.Message;
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.UUID;
 
+import slack.lite.entity.Message;
 import slack.lite.service.MessageService;
 
+
 @RestController
-// @RequestMapping("??")
+@RequestMapping("api/v1/messages/")
 public class MessageController {
 
 	@Autowired
 	MessageService messageService;
 
-	@PostMapping("messages")
+	@PostMapping
 	public void create(@RequestBody Message content) {
 		messageService.addMessage(content);
 	}
 
-	@GetMapping("messages")
+	@GetMapping
 	public List<Message> getMessages() {
 		return messageService.findAllMessages();
 	}
 
-	@GetMapping("messages/{id}")
+	@GetMapping("{id}")
 	public void deleteMessage(@PathVariable("id") UUID id) {
 		messageService.deleteMessage();
 	}
--- src/main/java/slack/lite/repository/MessageRepository.java
+++ src/main/java/slack/lite/repository/MessageRepository.java
@@ -1,11 +1,9 @@
 package slack.lite.repository;
 
-import java.util.List;
 import java.util.UUID;
 import slack.lite.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
-	List<Message> findByThreadByIndex();
 }
