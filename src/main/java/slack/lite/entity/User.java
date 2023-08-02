233d11e38fe519f1d5445c75149750d52854d707
Angelisium <angelisium@daum.net>
2023-08-02 15:01:53 +0200

    Create User.java

---
+++ src/main/java/slack/lite/entity/User.java
@@ -0,0 +1,34 @@
+package slack.lite.entity;
+
+import java.util.UUID;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "user", schema = "public")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	private String name;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
