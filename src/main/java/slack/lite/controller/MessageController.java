2884d8f8099ad36e11de94feeaa8eeb2cd81a306
Angelisium <angelisium@daum.net>
2023-08-02 18:20:31 +0200

    Update MessageController.java

--- src/main/java/slack/lite/controller/MessageController.java
+++ src/main/java/slack/lite/controller/MessageController.java
@@ -1,35 +1,45 @@
 package slack.lite.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import java.util.Set;
 import java.util.UUID;
-
 import slack.lite.entity.Message;
 import slack.lite.service.MessageService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class LoadingParameters {
+	public UUID id;
+	public Integer offset;
+}
 
 @RestController
 @RequestMapping("api/v1/messages")
 public class MessageController {
-
 	@Autowired
-	MessageService messageService;
-
-	@PostMapping
-	public void create(@RequestBody Message content) {
-		messageService.addMessage(content);
-	}
+	MessageService service;
 
 	@GetMapping
-	public List<Message> getMessages() {
-		return messageService.findAllMessages();
+	public Set<Message> load(@RequestBody LoadingParameters params) {
+		return service.load(params.id, params.offset);
 	}
 
-	
+	@PostMapping
+	public Message create(@RequestBody String content) {
+		Message msg = new Message();
+		msg.setContent(content);
+		// TODO : define author via http authentication header
+		return service.save(msg);
+	}
+
 	@DeleteMapping("{id}")
-	public void deleteMessage(@PathVariable("id") UUID id) {
-		messageService.deleteMessage(id);
+	public void delete(@PathVariable("id") UUID id) {
+		service.delete(id);
 	}
 }
