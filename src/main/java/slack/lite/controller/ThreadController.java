d880d4f2144ba224f1e5ce632062c501d7651e8c
Angelisium <angelisium@daum.net>
2023-08-03 11:20:51 +0200

    Update ThreadController.java

--- src/main/java/slack/lite/controller/ThreadController.java
+++ src/main/java/slack/lite/controller/ThreadController.java
@@ -2,11 +2,8 @@ package slack.lite.controller;
 
 import java.util.Set;
 import java.util.UUID;
-import java.util.Optional;
 import slack.lite.entity.Thread;
-import slack.lite.entity.Response;
 import slack.lite.service.ThreadService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,59 +22,26 @@ class ThreadLite {
 @RestController
 @RequestMapping("api/v1/threads")
 public class ThreadController {
-	private String bad_label = "bad label";
-	private String bad_id = "bad id";
-	private String not_found = "specified thread not found";
-	private String successfully_deleted = "thread deleted";
-
 	@Autowired
 	private ThreadService service;
 
 	@GetMapping
-	public ResponseEntity<Response> load() {
-		Set<Thread> thrds = service.load();
-		Response content = new Response(HttpStatus.OK, thrds);
-		return new ResponseEntity<>(content, HttpStatus.OK);
+	public ResponseEntity<Set<Thread>> load() {
+		return ResponseEntity.ok(service.load());
 	}
 
 	@PostMapping
-	public ResponseEntity<Response> create(@RequestBody ThreadLite th) {
-		String c = th.label;
-		if (c.trim().length() < 1) {
-			Response content = new Response(HttpStatus.BAD_REQUEST, bad_label);
-			return new ResponseEntity<>(content, HttpStatus.BAD_REQUEST);
-		}
-		Thread saved = service.save(c);
-		Response content = new Response(HttpStatus.OK, saved);
-		return new ResponseEntity<>(content, HttpStatus.OK);
+	public ResponseEntity<Thread> create(@RequestBody ThreadLite th) {
+		return ResponseEntity.of(service.save(th.label));
 	}
 
 	@PutMapping
-	public ResponseEntity<Response> update(@RequestBody Thread th) {
-		if (th.getId() == null) {
-			Response content = new Response(HttpStatus.BAD_REQUEST, bad_id);
-			return new ResponseEntity<>(content, HttpStatus.BAD_REQUEST);
-		}
-		if (th.getLabel().trim().length() < 1) {
-			Response content = new Response(HttpStatus.BAD_REQUEST, bad_label);
-			return new ResponseEntity<>(content, HttpStatus.BAD_REQUEST);
-		}
-		Optional<Thread> saved = service.save(th);
-		if (saved.isPresent()) {
-			Response content = new Response(HttpStatus.OK, saved.get());
-			return new ResponseEntity<>(content, HttpStatus.OK);
-		}
-		Response content = new Response(HttpStatus.BAD_REQUEST, not_found);
-		return new ResponseEntity<>(content, HttpStatus.BAD_REQUEST);
+	public ResponseEntity<Thread> update(@RequestBody Thread th) {
+		return ResponseEntity.of(service.save(th));
 	}
 
 	@DeleteMapping("{id}")
-	public ResponseEntity<Response> delete(@PathVariable("id") UUID id) {
-		if (service.delete(id)) {
-			Response content = new Response(HttpStatus.OK, successfully_deleted);
-			return new ResponseEntity<>(content, HttpStatus.OK);
-		}
-		Response content = new Response(HttpStatus.BAD_REQUEST, bad_id);
-		return new ResponseEntity<>(content, HttpStatus.BAD_REQUEST);
+	public ResponseEntity<Thread> delete(@PathVariable("id") UUID id) {
+		return ResponseEntity.of(service.delete(id));
 	}
 }
