5205de58cd7d6ce3c99fb91cf2195994b2359fe0
Angelisium <angelisium@daum.net>
2023-08-06 17:43:58 +0200

    Update Thread.java

--- src/main/java/slack/lite/entity/Thread.java
+++ src/main/java/slack/lite/entity/Thread.java
@@ -7,8 +7,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Entity
+@JsonInclude(Include.NON_NULL)
 @Table(name = "thread", schema = "public")
 public class Thread {
 	@Id
